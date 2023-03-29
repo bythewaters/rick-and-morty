9fb9904e5c688f79bc67e4ef86cfb3a320b8fd1b
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-29 15:01:16 +0200

    Added CharacterListView and characters endpoint

--- Rick_and_Morty/urls.py
+++ Rick_and_Morty/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("characters.urls"), namespace="characters"),
+    path("api/", include("characters.urls", namespace="characters")),
 ]
--- characters/views.py
+++ characters/views.py
@@ -1,7 +1,7 @@
 import random
 
 from django.shortcuts import render
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -12,9 +12,20 @@ from characters.serializers import CharacterSerializer
 
 @api_view(["GET"])
 def get_random_character(request: Request) -> Response:
-    pks = Character.objects.values("pk", flat=True)
+    pks = Character.objects.values_list("pk", flat=True)
     random_pk = random.choice(pks)
     random_character = Character.objects.get(pk=random_pk)
     serializer = CharacterSerializer(random_character)
 
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CharacterListView(generics.ListAPIView):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self):
+        queryset = Character.objects.all()
+        name = self.request.query_params.get("name")
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
