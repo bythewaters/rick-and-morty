c819a117e6ad8e4e4a608ffc93ea6b7091aa1c7f
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-29 15:22:10 +0200

    Added swagger documentation in characters app

--- Rick_and_Morty/settings.py
+++ Rick_and_Morty/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "drf_spectacular",
     "rest_framework",
     "characters",
 ]
@@ -122,6 +123,16 @@ STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Rick & Morty API",
+    "DESCRIPTION": "Documentations Characters",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 RICK_AND_MORTY_API_CHARACTERS_URL = "https://rickandmortyapi.com/api/character"
--- characters/views.py
+++ characters/views.py
@@ -1,6 +1,6 @@
 import random
 
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
@@ -10,8 +10,10 @@ from characters.models import Character
 from characters.serializers import CharacterSerializer
 
 
+@extend_schema(responses={status.HTTP_200_OK: CharacterSerializer})
 @api_view(["GET"])
 def get_random_character(request: Request) -> Response:
+    """Get random character from Rick & Morty world"""
     pks = Character.objects.values_list("pk", flat=True)
     random_pk = random.choice(pks)
     random_character = Character.objects.get(pk=random_pk)
@@ -29,3 +31,14 @@ class CharacterListView(generics.ListAPIView):
         if name is not None:
             queryset = queryset.filter(name__icontains=name)
         return queryset
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="name", description="Filter by name", required=False, type=str
+            )
+        ]
+    )
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        """List characters filter by name"""
+        return super().get(request, *args, **kwargs)
