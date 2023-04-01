c98076ded4611b60aa2e8b5d5e1138a7578892ae
Taras Martyniuk <bythewaters800@gmail.com>
2023-04-01 15:33:18 +0200

    Added celery with django-celery-beat shedule

--- characters/scraper.py
+++ characters/scraper.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 import requests
+from django.db import IntegrityError
 
 from characters.models import Character
 
@@ -29,7 +30,10 @@ def scraper_url() -> list[Character]:
 
 def save_character(characters: list[Character]) -> None:
     for character in characters:
-        character.save()
+        try:
+            character.save()
+        except IntegrityError:
+            print(f"Character with id {character.api_id} already exist")
 
 
 def sync_characters_with_api() -> None:
